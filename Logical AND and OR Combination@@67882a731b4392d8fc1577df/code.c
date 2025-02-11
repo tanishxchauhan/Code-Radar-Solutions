2e3b712e1254f32769d7e0de5378b88c90ad6084
tanishxchauhan <tanishc28105@gmail.com>
2025-02-11 23:58:01 +0530

    Solution to Logical AND and OR Combination

--- Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
-int main(){
-    int x ,y;
-    scanf("%d %d",&x,&y);
-    bool = (x>0) && (y<0) || (x==y,x==0);
-    printf("%d",bool);
-    return 0;
 
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    bool result = (x > 0) && (y < 0) || (x == 0);
+
+    printf("%s\n", result ? "true" : "false"); // Ternary operator
+
+    return 0;
 }
