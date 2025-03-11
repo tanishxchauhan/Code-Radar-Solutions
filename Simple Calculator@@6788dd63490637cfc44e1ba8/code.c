1f4c6dd42e4af6f8318f0e80a147d45a7620fb26
Tanish Chauhan <tanish1824.becse24@chitkara.edu.in>
2025-03-12 00:04:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
-int main(){
-    int x,y,op;
-    scanf("%d %d %d",x,y,op);
-    if(op=="+"){
-        printf("%d",x+y);
-    }
-    if(op=="-"){
-        printf("%d",x-y);
-    }
-    if(op=="*"){
-        printf("%d",x*y);
-    }
-    if(op=="/"){
-        printf("%d",x/y);
+#include <string.h> // Include string.h for strcmp
+
+int main() {
+    int x, y;
+    char op[2]; // Use a character array to store the operator
+
+    scanf("%d %d %1s", &x, &y, op); // Corrected scanf format specifier
+
+    if (strcmp(op, "+") == 0) { // Use strcmp to compare strings
+        printf("%d", x + y);
+    } else if (strcmp(op, "-") == 0) {
+        printf("%d", x - y);
+    } else if (strcmp(op, "*") == 0) {
+        printf("%d", x * y);
+    } else if (strcmp(op, "/") == 0) {
+        if(y == 0){
+            printf("Division by zero is not allowed.");
+        }else{
+            printf("%d", x / y);
+        }
+    } else {
+        printf("Invalid operator");
     }
 
     return 0;
