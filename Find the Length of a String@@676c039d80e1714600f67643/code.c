2a7d3e2c4ecab91cb58c7e181de01a9b332c96c5
Tanish Chauhan <tanish1824.becse24@chitkara.edu.in>
2025-03-21 11:32:11 +0530

    Solution to Find the Length of a String

--- Find the Length of a String@@676c039d80e1714600f67643/code.c
+++ Find the Length of a String@@676c039d80e1714600f67643/code.c
@@ -1,50 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
 int main() {
     char str[1000];
     fgets(str, sizeof(str), stdin);
 
-    int wordCount = 0;
-    int inWord = 0;
+    int length = 0;
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
 
-    // Remove trailing newline if present
-    int len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0';
+    }
+    if(str[]=='0'){
+        printf('0')
     }
 
-    // Check if the string is empty or contains only whitespace
-    int isOnlyWhitespace = 1; // Assume it's whitespace only
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (!isspace(str[i])) {
-            isOnlyWhitespace = 0; // Found a non-whitespace character
-            break;
-        }
-    }
-
-    if (isOnlyWhitespace || strlen(str) == 0) {
-        printf("0\n"); // Output 0 for empty or whitespace-only
-        return 0;
-    }
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (isspace(str[i])) {
-            if (inWord) {
-                wordCount++;
-                inWord = 0;
-            }
-        } else {
-            inWord = 1;
-        }
-    }
-
-    if (inWord) {
-        wordCount++;
-    }
-
-    printf("%d\n", wordCount);
+    printf("%d\n", length);
 
     return 0;
 }
