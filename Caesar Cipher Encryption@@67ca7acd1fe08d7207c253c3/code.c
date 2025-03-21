2f860ca975340ba3dfcb14fc2b09a1ca73c870f3
Tanish Chauhan <tanish1824.becse24@chitkara.edu.in>
2025-03-21 10:58:52 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void caesarCipher(char message, int shift, char encrypted) {
+void caesarCipher(char message[], int shift, char encrypted[]) {
     int i = 0;
     while (message[i] != '\0') {
         char ch = message[i];
@@ -14,28 +14,4 @@ void caesarCipher(char message, int shift, char encrypted) {
         i++;
     }
     encrypted[i] = '\0'; // Null-terminate the encrypted string
-}
-
-int main() {
-    char message[100];
-    int shift;
-    char encrypted[100]; // Store the encrypted message
-
-    // Read the input message
-    scanf("%[^\n]%*c", message); // Read entire line including spaces
-
-    // Read the shift value
-    scanf("%d", &shift);
-
-    // Normalize the shift to the range [0, 25]
-    shift = shift % 26;
-    if (shift < 0) shift = shift + 26; // Handle negative shifts
-
-    // Call the caesarCipher function
-    caesarCipher(message, shift, encrypted);
-
-    // Output the encrypted message
-    printf("%s\n", encrypted);
-
-    return 0;
 }
