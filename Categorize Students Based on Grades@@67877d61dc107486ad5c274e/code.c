1b3e025a5c1f39ecf994c15a64f8340b076d8cb5
Tanish Chauhan <tanish1824.becse24@chitkara.edu.in>
2025-03-21 13:57:46 +0530

    Solution to Categorize Students Based on Grades

--- Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
 struct Student {
-    int roll;
+    int roll_number;
     char name[50];
     float marks;
+    char grade;
 };
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    struct Student students[n];
+// Function to assign grade based on marks
+char getGrade(float marks) {
+    if (marks >= 85)
+        return 'A';
+    else if (marks >= 70)
+        return 'B';
+    else
+        return 'C';
+}
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d %s %f", &students[i].roll, students[i].name, &students[i].marks);
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    struct Student students[N];
+
+    // Input student data
+    for (int i = 0; i < N; i++) {
+        scanf("%d %s %f", &students[i].roll_number, students[i].name, &students[i].marks);
+        students[i].grade = getGrade(students[i].marks);
     }
 
-    for (int i = 0; i < n; i++) {
-        char grade;
-        if (students[i].marks >= 85) {
-            grade = 'A';
-        } else if (students[i].marks >= 70 && students[i].marks <= 84) {
-            grade = 'B';
-        } else {
-            grade = 'C';
-        }
-        printf("Roll Number: %d, Name: %s, Grade: %c\n", students[i].roll, students[i].name, grade);
+    // Output student data
+    for (int i = 0; i < N; i++) {
+        printf("Roll Number: %d, Name: %s, Grade: %c\n", students[i].roll_number, students[i].name, students[i].grade);
     }
 
     return 0;
