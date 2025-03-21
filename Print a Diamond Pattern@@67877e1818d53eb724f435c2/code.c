c037078af1e0738f051a7e66d75e90e84485f3b3
Tanish Chauhan <tanish1824.becse24@chitkara.edu.in>
2025-03-21 10:32:16 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -3,13 +3,10 @@
 int main() {
     int rows;
 
-    printf("Enter the number of rows (odd): ");
+
     scanf("%d", &rows);
 
-    if (rows % 2 == 0) {
-        printf("Please enter an odd number of rows.\n");
-        return 1; // Indicate an error
-    }
+
 
     // Upper half of the diamond
     for (int i = 1; i <= rows / 2 + 1; i++) {
